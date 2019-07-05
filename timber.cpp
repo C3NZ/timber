8ba102a733011a244514896902dec7fd8632079a
C3NZ <cenz@cenz.io>
2019-07-05 15:49:31 -0700

    [feat] refactor code and create a RenderWindow object named window

--- timber.cpp
+++ timber.cpp
@@ -3,7 +3,9 @@
 using namespace sf;
 
 int main() {
-  VideoMode vm(1920, 1081);
-  Window(vm, "Timber!!!", Style::Fullscreen);
+  // Set the video mode to 1920x1080, the dimension of our screen.
+  VideoMode vm(1920, 1080);
+  // Create a render window that will render our videomode
+  RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
   return 0;
 }
