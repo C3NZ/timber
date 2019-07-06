83f9dad1797168fb117b31d2413ab184ccd89c64
C3NZ <cenz@cenz.io>
2019-07-06 01:17:01 -0700

    [feat] add in bees, trees, and cloud texture

--- timber.cpp
+++ timber.cpp
@@ -9,18 +9,55 @@ int main() {
   RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 
   // Create our texture for loading imgs into the GPU
-  Texture backgroundTexture;
-  // Create a sprite
-  Sprite backgroundSprite;
+  // Add the tree texture
+  // Bee texture
+  // Create sprites for both the background and the tree.
 
-  // Load the texture from the png file
+  // Load the background texture and load it's sprite on screen
+  Texture backgroundTexture;
+  Sprite backgroundSprite;
   backgroundTexture.loadFromFile("graphics/background.png");
-  // Associate the sprite to the texture
   backgroundSprite.setTexture(backgroundTexture);
-  // Set the sprites position relative to the inside of the window
   backgroundSprite.setPosition(0, 0);
 
-  // The main while loop
+  // Create the tree texture and load it's sprite on screen
+  Texture treeTexture;
+  Sprite treeSprite;
+  treeTexture.loadFromFile("graphics/tree.png");
+  treeSprite.setTexture(treeTexture);
+  treeSprite.setPosition(810, 0);
+  // Set the initial positions for the sprites
+  // relative to the window.
+
+  Texture beeTexture;
+  Sprite beeSprite;
+  beeTexture.loadFromFile("graphics/bee.png");
+  beeSprite.setTexture(beeTexture);
+  beeSprite.setPosition(0, 800);
+
+  // Bee variables
+  bool beeActive = false;
+  float beeSpeed = 0.0f;
+
+  Texture cloudTexture;
+  cloudTexture.loadFromFile("graphics/cloud.png");
+
+  Sprite cloudSprite1;
+  Sprite cloudSprite2;
+  Sprite cloudSprite3;
+
+  cloudSprite1.setTexture(cloudTexture);
+  cloudSprite2.setTexture(cloudTexture);
+  cloudSprite3.setTexture(cloudTexture);
+
+  cloudSprite1.setPosition(0, 0);
+  cloudSprite2.setPosition(0, 250);
+  cloudSprite3.setPosition(0, 500);
+
+  float cloud1Speed = 0.0f;
+  float cloud2Speed = 0.0f;
+  float cloud3Speed = 0.0f;
+
   while (window.isOpen()) {
     /*
      * Handle player input
@@ -44,6 +81,16 @@ int main() {
     // Draw the background sprite
     window.draw(backgroundSprite);
 
+    // Draw the clouds
+    window.draw(cloudSprite1);
+    window.draw(cloudSprite2);
+    window.draw(cloudSprite3);
+
+    // Draw the trees
+    window.draw(treeSprite);
+
+    // Draw the bee
+    window.draw(beeSprite);
     // Display everything drawn
     window.display();
   }
